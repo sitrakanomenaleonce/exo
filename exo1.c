e50dd5da745b9297e9ed5fc3870dd109f7a79a49
sitrakanomenaleonce <hsitrakanomenaleonce@gmail.com>
2026-03-16 14:21:01 +0300

    Update and rename README.md to exo1.c

---
+++ exo1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,r;
+    printf("entrer la premiere valeur:");
+    scanf("%d",&a);
+
+    printf("entrer la deuxieme valeur:");
+    scanf("%d",&b);
+
+    r = a+b ;
+
+    printf("resultat de la somme est: %d ",c);
+
+    return 0;
+}
