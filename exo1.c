302aa0e03a0644b09f88cdc29f65ddeb9cb690b4
sitrakanomenaleonce <hsitrakanomenaleonce@gmail.com>
2026-03-16 16:16:27 +0300

    Update exo1.c

--- exo1.c
+++ exo1.c
@@ -3,7 +3,8 @@
 
 bool estPremier(int n) {
     if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 2; i * i <= n; i++) 
+    {
         if (n % i == 0) return false;
     }
     return true;
@@ -11,8 +12,10 @@ bool estPremier(int n) {
 
 int main() {
     int count = 0, num = 2;
-    while (count < 25) {
-        if (estPremier(num)) {
+    while (count < 25)
+        {
+        if (estPremier(num)) 
+        {
             printf("%d ", num);
             count++;
         }
