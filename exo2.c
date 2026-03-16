1ec95e3b1662474ed6df4dfc1ffb297068bf33e8
sitrakanomenaleonce <hsitrakanomenaleonce@gmail.com>
2026-03-16 16:35:58 +0300

    Update exo2.c

--- exo2.c
+++ exo2.c
@@ -5,22 +5,30 @@ int main() {
     printf("Taille des vecteurs: ");
     scanf("%d", &n);
 
-    int A[n], B[n];
+    int A[n], B[n],S[n];
     printf("Entrez les éléments du vecteur A:\n");
-    for (int i = 0; i < n; i++) scanf("%d", &A[i]);
+    
+    for (int i = 0; i < n; i++)
+        scanf("%d", &A[i]);
 
     printf("Entrez les éléments du vecteur B:\n");
-    for (int i = 0; i < n; i++) scanf("%d", &B[i]);
+    
+    for (int i = 0; i < n; i++) 
+        scanf("%d", &B[i]);
 
     int produitScalaire = 0;
-    printf("Somme des vecteurs: ");
     for (int i = 0; i < n; i++) 
     {
         produitScalaire += A[i] * B[i];
-        printf("%d ", A[i] + B[i]);
+        S[i]= A[i] + B[i];
+        
     }
 
-    printf("\nProduit scalaire = %d\n", produitScalaire);
+    printf("\nProduit scalaire de deux vecteurs = %d \n", produitScalaire);
+    printf("Contenu du somme de deux vecteurs : ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", S[i]);
+    }
     return 0;
 }
 
